902e0662abfb580ff6200318dfc4c43dec2b9d8a
Phạm Hải <sonhai0803@gmail.com>
2025-03-27 09:25:33 +0700

    retype

--- biz_play_crawler/parser/parse_event_detail.py
+++ biz_play_crawler/parser/parse_event_detail.py
@@ -23,7 +23,11 @@ def parse_event_detail(event :CrawlEvent[None, Event, HtmlResponse],
     except IndexError:
         category_url = None 
     category_text = response.xpath(f"//a[@itemid='{category_url}']/span/text()").get()
-    data.event_category = Category(text=category_text, href=category_url)
+    category_dict = {}
+    category_dict["text"] = category_text
+    category_dict["href"] = category_url
+    data.event_category = category_dict
+    
     video_title = response.xpath("//span[@class='title mr-2']/text()").getall()
     video_timeline = response.xpath("//span[@class='time']/text()").getall()
     
@@ -32,20 +36,20 @@ def parse_event_detail(event :CrawlEvent[None, Event, HtmlResponse],
         event_video_content.append(VideoTimeLine(title=title, time_at=time_at))
     data.event_video_content = event_video_content if event_video_content else None
     
-    description_title =  " ".join(response.xpath("normalize-space(//div[@class='description']//strong//text())").getall())
-    description_content = " ".join(response.xpath("//div[@class='description']//p//text()").getall())
-    data.event_description = Description(title=description_title, content=description_content)
+    data.event_description_title =  " ".join(response.xpath("normalize-space(//div[@class='description']//strong//text())").getall())
+    data.event_description_content = " ".join(response.xpath("//div[@class='description']//p//text()").getall())
+    
     
     data.event_company_name = response.xpath("//li[@class='company']/div/text()").get()
     
     instructors = response.xpath("//div[@class='instructor']/div[@class='profile-text text-gray-200']")
     instructor_list = []
     for instructor in instructors:
-        corporate = instructor.xpath("./div[1]/text()").get()
+        company = instructor.xpath("./div[1]/text()").get()
         position = instructor.xpath("./div[2]/text()").get()
         name = instructor.xpath("./div[3]/text()").get()
         description = " ".join(instructor.xpath("./div[4]//p/text()").getall())
-        instructor_list.append(Speaker(corporate=corporate, position=position, name=name, description=description))
+        instructor_list.append(Speaker(company=company, position=position, name=name, description=description))
     data.event_speaker = instructor_list if instructor_list else None
     
     related_url = list(set(response.xpath("//div[@class='wrapper']//a/@href").getall()))
@@ -54,16 +58,20 @@ def parse_event_detail(event :CrawlEvent[None, Event, HtmlResponse],
     book_information = response.xpath("//div[@class='book']/div[@class='information']")
     if book_information is not None:
         for book in book_information:
-            book_title = book.xpath(".//div[@class='title']//h2/text()").get()
-            author = book.xpath(".//div[@class='subtitle author']//h2/text()").get()
-            publish_date = book.xpath(".//div[@class='subtitle publication-date']/text()").get().replace("発売日：", "").strip()
-            publisher = book.xpath(".//div[@class='subtitle manufacturer']/text()").get().replace("出版社：", "").strip()
-            order_link = related_url
-            data.event_book_information = BookInformation(book_title=book_title, author=author, publish_date=publish_date, publisher=publisher, order_link=order_link)
+            data.event_book_title = book.xpath(".//div[@class='title']//h2/text()").get()
+            data.event_book_author = book.xpath(".//div[@class='subtitle author']//h2/text()").get()
+            data.event_book_publish_date = book.xpath(".//div[@class='subtitle publication-date']/text()").get().replace("発売日：", "").strip()
+            data.event_book_publisher = book.xpath(".//div[@class='subtitle manufacturer']/text()").get().replace("出版社：", "").strip()
+            data.event_book_order_link = related_url            
             data.event_related_url = None
     else:
         data.event_related_url = related_url
-        data.event_book_information = None
+        data.event_book_title = None
+        data.event_book_author = None
+        data.event_book_publish_date = None
+        data.event_book_publisher = None
+        data.event_book_order_link = None
+        
     
     other_seminar_urls = list(set(response.xpath("//section[@class='sidebar-seminars']//a/@href").getall()))
     
--- biz_play_crawler/schema/biz_play_event.py
+++ biz_play_crawler/schema/biz_play_event.py
@@ -2,39 +2,34 @@ from pydantic import BaseModel
 from typing import Optional, List
 
 
-class Category(BaseModel):
-    text: Optional[str] = None
-    href: Optional[str] = None
-    
+
 class VideoTimeLine(BaseModel):
     title: Optional[str] = None
     time_at: Optional[str] = None
  
-class Description(BaseModel):
-    title: Optional[str] = None
-    content: Optional[str] = None
-
 class Speaker(BaseModel):
-    corporate: Optional[str] = None
+    company: Optional[str] = None
     position: Optional[str] = None
     name: Optional[str] = None
     description: Optional[str] = None
 
-class BookInformation(BaseModel):
-    book_title: Optional[str] = None
-    author: Optional[str] = None
-    publish_date: Optional[str] = None
-    publisher: Optional[str] = None
-    order_link: List[str] = None
+
 
 class BizPlayEvent(BaseModel):
     event_id: Optional[str] = None
     source_event_url: Optional[str] = None
     event_name: Optional[str] = None
-    event_category : Optional[Category] = None
+    event_category : Optional[dict] = None
     event_video_content: Optional[List[VideoTimeLine]] = None
-    event_description: Optional[Description] = None
+    event_description_title: Optional[str] = None
+    event_description_content: Optional[str] = None
     event_company_name: Optional[str] = None
     event_speaker: Optional[List[Speaker]] = None
     event_related_url: Optional[List[str]] = None
-    event_book_information: Optional[BookInformation] = None
+  
+    event_book_title: Optional[str] = None
+    event_book_author: Optional[str] = None
+    event_book_publish_date: Optional[str] = None
+    event_book_publisher: Optional[str] = None
+    event_book_order_link: List[str] = None
+  
